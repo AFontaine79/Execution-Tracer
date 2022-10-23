ee7f6fccd1097f3b19099054bd857f99a7961d67
Aaron Fontaine <AFontaine79@yahoo.com>
2022-10-23 15:25:47 -0500

    fixup

--- lib/include/execution_tracer_conf.h
+++ lib/include/execution_tracer_conf.h
@@ -16,28 +16,26 @@
 /* Standard Arm Cortex FLASH base */
 // #define FLASH_BASE		(0x00000000)
 /* Standard STM32 FLASH base */
-#define FLASH_BASE		(0x08000000)
+#define FLASH_BASE (0x08000000)
 
 /**
  * Variable and SFR addresses are also stored as offsets.
-*/
+ */
 /* Standard Arm Cortex RAM base */
-#define RAM_BASE		(0x20000000)
+#define RAM_BASE (0x20000000)
 /* Standard STM32 peripheral registers region base address */
-#define SFR_BASE		(0x40000000)
-
+#define SFR_BASE (0x40000000)
 
 /**
  * Execution tracer settings.
  */
 
-#define STOP_TRACING_AFTER_RESET		1
+#define STOP_TRACING_AFTER_RESET        1
 
-#define USE_NOINIT_RAM_FOR_TRACING		1
+#define USE_NOINIT_RAM_FOR_TRACING      1
 
-#define ALLOW_OVERWRITE					1
+#define ALLOW_OVERWRITE                 1
 
-#define BUFFER_LENGTH_IN_WORDS			1024
-
+#define BUFFER_LENGTH_IN_WORDS          1024
 
 #endif /* LIB_INCLUDE_EXECUTION_TRACER_CONF_H_ */
