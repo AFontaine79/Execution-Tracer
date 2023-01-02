93f6548aaa1a9882d1297386931e67a69e4b84d8
Aaron Fontaine <AFontaine79@yahoo.com>
2023-01-02 13:05:20 -0600

    Updates to SVD parser
    Based on testing with LoRaMac-node project.

--- tools/parse-svd.py
+++ tools/parse-svd.py
@@ -10,7 +10,7 @@ class PeripheralRegister:
         self.peripheral_name = ""
         self.register_name = ""
 
-def get_mcu_register_set(device):
+def get_mcu_register_set_for_device(device):
     registers = {}
     for peripheral in device.peripherals:
         peripheral_name = peripheral.name
@@ -28,7 +28,7 @@ def get_mcu_register_set_for_packaged_svd(make, model):
     try:
         parser = SVDParser.for_packaged_svd(make, model + '.svd')
         device = parser.get_device()
-        return get_mcu_register_set(device)
+        return get_mcu_register_set_for_device(device)
     except FileNotFoundError as e:
         print(f"{model} by {make}: Device not found")
         return None
@@ -41,39 +41,41 @@ def get_mcu_register_set_from_xml_file(file_name):
     try:
         parser = SVDParser.for_xml_file(file_name)
         device = parser.get_device()
-        return get_mcu_register_set(device)
+        return get_mcu_register_set_for_device(device)
     except Exception as e:
         print(e, file=sys.stderr)
         return None
 
-def main():
-    global svd_file
-    global make
-    global model
-    global registers
+def get_mcu_register_set(svd_file = None, make = None, model = None):
+    if not make and not model and not svd_file:
+        print("WARNING: No arguments for SVD selection")
+        return None
 
+    if (svd_file and (make or model)) or (make and not model) or (model and not make):
+        print("WARNING: Invalid arguments combination for SVD selection")
+        print("Provide MAKE and MODEL to use a packaged svd file")
+        print("Provide SVD_FILE to use an external svd file")
+        print("Do not use both at the same time; They are mutually exclusive")
+        return None
+
+    if make and model:
+        return get_mcu_register_set_for_packaged_svd(make, model)
+    else:
+        return get_mcu_register_set_from_xml_file(svd_file)
+
+def main():
     parser = argparse.ArgumentParser(description='SVD Parser')
-    parser.add_argument('--file', help='SVD file in XML foramt', type=str, required=False)
+    parser.add_argument('--svd_file', help='SVD file in XML foramt', type=str, required=False)
     parser.add_argument('--make', help='Vendor (e.g. Atmel or STMicro)', type=str, required=False)
     parser.add_argument('--model', help='Device name (e.g. ATSAMA5D33 or STM32L4x6)', type=str, required=False)
     args = parser.parse_args()
 
-    svd_file = args.file
+    svd_file = args.svd_file
     make = args.make
     model = args.model
 
-    if (svd_file and (make or model)) or (make and not model) or (model and not make):
-        print("Invalid arguments combination")
-        print("Provide MAKE and MODEL to use a packaged svd file")
-        print("Provide FILE to use an external svd file")
-        print("Do not use both at the same time; They are mutually exclusive")
-        sys.exit(2)
+    registers = get_mcu_register_set(svd_file, make, model)
 
-    if make and model:
-        registers = get_mcu_register_set_for_packaged_svd(make, model)
-    else:
-        registers = get_mcu_register_set_from_xml_file(svd_file)
-    
     if registers:
         for key in registers.keys():
             peripheral_register = registers[key]
