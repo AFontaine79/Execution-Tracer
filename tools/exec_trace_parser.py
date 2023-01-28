6d0ad0c1a46a60a13350e51322f3279e3d77bbcb
Aaron Fontaine <AFontaine79@yahoo.com>
2023-01-28 11:28:55 -0600

    Refactored trace parsing functions into its own module and class definition

---
+++ tools/exec_trace_parser.py
@@ -0,0 +1,91 @@
+
+class ExecTraceParser:
+    def __init__(self, functions, variables, registers):
+        self.functions = functions
+        self.variables = variables
+        self.registers = registers
+        self.indent_level = 0
+
+    def set_flash_base(self, flash_base):
+        self.FLASH_BASE = flash_base
+
+    def set_ram_base(self, ram_base):
+        self.RAM_BASE = ram_base
+
+    def set_sfr_base(self, sfr_base):
+        self.SFR_BASE = sfr_base
+
+    def inc_indent(self):
+        self.indent_level = self.indent_level + 1
+
+    def dec_indent(self):
+        if self.indent_level > 0:
+            self.indent_level = self.indent_level - 1
+
+    def print_indent(self):
+        for i in range(0, self.indent_level):
+            print("  ", end="")
+
+    def reset_indent(self):
+        self.indent_level = 0
+
+    def get_func_name(self, value):
+        func_addr = (value & 0xFFFFFFE) + self.FLASH_BASE
+        if func_addr in self.functions.keys():
+            return self.functions[func_addr]
+        else:
+            return "Function @ 0x%08X" % func_addr
+
+    def get_var_name(self, value):
+        var_addr = (value & 0xFFFFFFE) + self.RAM_BASE
+        if var_addr in self.variables.keys():
+            return self.variables[var_addr]
+        else:
+            return "Variable @ 0x%08X" % var_addr
+
+    def get_sfr_name(self, value):
+        sfr_addr = (value & 0xFFFFFFE) + self.SFR_BASE
+        if sfr_addr in self.registers.keys():
+            peripheral_register = self.registers[sfr_addr]
+            return "%s->%s" % (peripheral_register.peripheral_name, peripheral_register.register_name)
+        else:
+            return "SFR @ 0x%08X" % sfr_addr
+
+    def trace_version(self, value):
+        ver_char = (value >> 16) & 0xFF
+        ver_major = (value >> 8) & 0xFF
+        ver_minor = (value >> 0) & 0xFF
+        self.reset_indent()
+        print("Tracer version %c%d.%d" % (ver_char, ver_major, ver_minor))
+
+    def trace_reset(self, value):
+        self.print_indent()
+        print("Processor reset: 0x%02X" % value)
+
+    def trace_func_entry(self, value):
+        self.print_indent()
+        print("Enter %s" % self.get_func_name(value))
+        # Increment indent after Enter statement
+        # This is like an opening brace
+        self.inc_indent()
+
+    def trace_func_exit(self, value):
+        # Decrement indent before Exit statement
+        # This is like a closing brace
+        self.dec_indent()
+        self.print_indent()
+        print("Exit %s" % self.get_func_name(value))
+
+    def trace_file_and_line(self, value):
+        module_num = (value >> 16) & 0xFFF
+        line_num = (value >> 0) & 0xFFFF
+        self.print_indent()
+        print("Module: %u, Line: %u" % (module_num, line_num))
+
+    def trace_variable(self, addr_value, var_value):
+        self.print_indent()
+        print("%s = %d" % (self.get_var_name(addr_value), var_value))
+
+    def trace_sfr(self, addr_value, reg_value):
+        self.print_indent()
+        print("%s = 0x%08X" % (self.get_sfr_name(addr_value), reg_value))
--- tools/live_trace.py
+++ tools/live_trace.py
@@ -1,6 +1,7 @@
 
 from parse_map_file import read_gnu_map_file
 from parse_svd import get_mcu_register_set
+from exec_trace_parser import ExecTraceParser
 
 import argparse
 import serial
@@ -10,88 +11,12 @@ FLASH_BASE = 0x08000000
 RAM_BASE   = 0x20000000
 SFR_BASE   = 0x40000000
 
-indent_level = 0
-
-def inc_indent():
-    global indent_level
-    indent_level = indent_level + 1
-
-def dec_indent():
-    global indent_level
-    if indent_level > 0:
-        indent_level = indent_level - 1
-
-def print_indent():
-    global indent_level
-    for i in range(0, indent_level):
-        print("  ", end="")
-
-def reset_indent():
-    global indent_level
-    indent_level = 0
-
-def get_func_name(value, functions):
-    func_addr = (value & 0xFFFFFFE) + FLASH_BASE
-    if func_addr in functions.keys():
-        return functions[func_addr]
-    else:
-        return "Function @ 0x%08X" % func_addr
-
-def get_var_name(value, variables):
-    var_addr = (value & 0xFFFFFFE) + RAM_BASE
-    if var_addr in variables.keys():
-        return variables[var_addr]
-    else:
-        return "Variable @ 0x%08X" % var_addr
-
-def get_sfr_name(value, registers):
-    sfr_addr = (value & 0xFFFFFFE) + SFR_BASE
-    if sfr_addr in registers.keys():
-        peripheral_register = registers[sfr_addr]
-        return "%s->%s" % (peripheral_register.peripheral_name, peripheral_register.register_name)
-    else:
-        return "SFR @ 0x%08X" % sfr_addr
-
-def trace_version(value):
-    ver_char = (value >> 16) & 0xFF
-    ver_major = (value >> 8) & 0xFF
-    ver_minor = (value >> 0) & 0xFF
-    reset_indent()
-    print("Tracer version %c%d.%d" % (ver_char, ver_major, ver_minor))
-
-def trace_reset(value):
-    print_indent()
-    print("Processor reset: 0x%02X" % value)
-
-def trace_func_entry(value, functions):
-    print_indent()
-    print("Enter %s" % get_func_name(value, functions))
-    # Increment indent after Enter statement
-    # This is like an opening brace
-    inc_indent()
-
-def trace_func_exit(value, functions):
-    # Decrement indent before Exit statement
-    # This is like a closing brace
-    dec_indent()
-    print_indent()
-    print("Exit %s" % get_func_name(value, functions))
-
-def trace_file_and_line(value):
-    module_num = (value >> 16) & 0xFFF
-    line_num = (value >> 0) & 0xFFFF
-    print_indent()
-    print("Module: %u, Line: %u" % (module_num, line_num))
-
-def trace_variable(addr_value, var_value, variables):
-    print_indent()
-    print("%s = %d" % (get_var_name(addr_value, variables), var_value))
-
-def trace_sfr(addr_value, reg_value, registers):
-    print_indent()
-    print("%s = 0x%08X" % (get_sfr_name(addr_value, registers), reg_value))
-
 def live_trace(ser, functions, variables, registers):
+    tracer = ExecTraceParser(functions, variables, registers)
+    tracer.set_flash_base(FLASH_BASE)
+    tracer.set_ram_base(RAM_BASE)
+    tracer.set_sfr_base(SFR_BASE)
+
     while(True):
         line = ser.read_until()
         line = line.decode(encoding='utf-8')
@@ -99,25 +24,25 @@ def live_trace(ser, functions, variables, registers):
             value = int(line, 0)
             idcode = (value >> 28) & 0xF
             if idcode == 1:
-                trace_version(value)
+                tracer.trace_version(value)
             elif idcode == 2:
-                trace_reset(value)
+                tracer.trace_reset(value)
             elif idcode == 3:
-                trace_func_entry(value, functions)
+                tracer.trace_func_entry(value)
             elif idcode == 4:
-                trace_func_exit(value, functions)
+                tracer.trace_func_exit(value)
             elif idcode == 5:
-                trace_file_and_line(value)
+                tracer.trace_file_and_line(value)
             elif idcode == 6:
                 line2 = ser.read_until()
                 line2 = line2.decode(encoding='utf-8')
                 value2 = int(line2, 0)
-                trace_variable(value, value2, variables)
+                tracer.trace_variable(value, value2)
             elif idcode == 7:
                 line2 = ser.read_until()
                 line2 = line2.decode(encoding='utf-8')
                 value2 = int(line2, 0)
-                trace_sfr(value, value2, registers)
+                tracer.trace_sfr(value, value2)
         else:
             print(line)
 
