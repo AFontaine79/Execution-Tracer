c57f2423bfe935300290f13203cb09cdc2c4e19c
Aaron Fontaine <AFontaine79@yahoo.com>
2023-02-05 11:51:29 -0600

    Added doc strings to trace_from_ser_port.py and trace_from_file.py

--- tools/trace_from_file.py
+++ tools/trace_from_file.py
@@ -1,4 +1,19 @@
+"""Parse and display the contents of an execution trace log saved to a text
+file.
 
+You must provide the map file and SVD information needed for translating trace
+values on the command line. You must also provide the path to the log file
+where traces are saved.
+
+The log file should likely be the recording from a terminal interface (such as
+RTT or serial port) to an execution tracer back-end.
+
+Limitations (and areas for future work):
+  - Raw trace buffer data is not supported. Every value must be
+    formatted with either "0x%X\n" or "%u\n" printf format strings.
+  - Flash, RAM and peripheral register base addresses are all hard-coded for
+    STMicro.
+"""
 from parse_map_file import read_gnu_map_file
 from parse_svd import get_mcu_register_set
 from exec_trace_parser import ExecTraceParser, TraceReaderInterface
@@ -11,12 +26,31 @@ FLASH_BASE = 0x08000000
 RAM_BASE   = 0x20000000
 SFR_BASE   = 0x40000000
 
-# TODO: Figure out how we can properly iterate the text file
 class TextFileTraceReader(TraceReaderInterface):
+    """Read trace buffer values saved to a log file in text format.
+    
+    Each value must be in one of the following two formats and terminated with
+    a new line character.
+      hex format: 0xXXXXXXXX
+      decimal format: XXXXXXXXX
+    
+    The log file must not contain anything other than trace buffer data.
+    """
     def __init__(self, log_file: io.StringIO):
+        """Initializes the log file trace reader.
+        
+        Args:
+          log_file: A text stream reader object returned by open().
+        """
         self.log_file = log_file
 
     def read_next(self) -> int:
+        """Read the next value from the log file and return it as an integer.
+        
+        Returns:
+          The next value from the log file or
+          TraceReaderInterface.END_OF_TRACE_BUFFER if there are no more values.
+        """
         line = self.log_file.readline()
         if len(line) > 0:
             value = int(line, 0)
@@ -25,6 +59,22 @@ class TextFileTraceReader(TraceReaderInterface):
         return value
 
 def live_trace(reader, functions, variables, registers):
+    """Parse all values from the log file and output to stdout.
+    
+    Iterates over the entire log file, translating all trace values to human
+    readable format and outputting the results to stdout.
+
+    The user must use ^C to terminate this function.
+
+    Args:
+      reader: A TextFileTraceReader object for retrieving trace buffer
+              values.
+      functions: Dictionary that maps MCU addresses to function names.
+      variables: Dictionary that maps MCU addresses to variable names.
+      registers: Dictionary that maps MCU addresses to
+                 parse_svd.PeripheralRegister objects.
+  
+    """
     tracer = ExecTraceParser(functions, variables, registers)
     tracer.set_flash_base(FLASH_BASE)
     tracer.set_ram_base(RAM_BASE)
@@ -32,6 +82,11 @@ def live_trace(reader, functions, variables, registers):
     tracer.read_and_trace_all(reader)
 
 def main():
+    """Parse a log file saved from a back-end terminal and output the results
+    to stdout.
+    
+    See module comment for usage.
+    """
     global ser
 
     parser = argparse.ArgumentParser(description='GNU Map file parser')
@@ -72,10 +127,12 @@ def main():
         live_trace(reader, functions, variables, registers)
 
 class InputError(RuntimeError):
+    """Boilerplate code for using this file directly from the command line."""
     def __init__(self, e):
         super(InputError, self).__init__(e)
 
 if __name__ == '__main__':
+    """Boilerplate code for using this file directly from the command line."""
     try:
         main()
     except InputError as e:
--- tools/trace_from_ser_port.py
+++ tools/trace_from_ser_port.py
@@ -1,4 +1,18 @@
+"""Start a live exeuction trace for an embedded target using the selected
+serial port.
 
+You must provide the map file and SVD information needed for translating trace
+values on the command line. You must also specify the serial device. This will
+be of the form 'COMn' on Windows or '/dev/ttyn' on Mac or Linux.
+
+Limitations (and areas for future work):
+  - The serial port is operated at 921600 with no flow control. There are no
+    command-line options to modify this.
+  - Raw UINT values on the serial port are not supported. Every value must be
+    formatted with either "0x%X\n" or "%u\n" printf format strings.
+  - Flash, RAM and peripheral register base addresses are all hard-coded for
+    STMicro.
+"""
 from parse_map_file import read_gnu_map_file
 from parse_svd import get_mcu_register_set
 from exec_trace_parser import ExecTraceParser, TraceReaderInterface
@@ -12,16 +26,57 @@ RAM_BASE   = 0x20000000
 SFR_BASE   = 0x40000000
 
 class SerialPortTraceReader(TraceReaderInterface):
+    """Read trace buffer values from a serial port back end.
+    
+    Each value must be in one of the following two formats and terminated with
+    a new line character.
+      hex format: 0xXXXXXXXX
+      decimal format: XXXXXXXXX
+    
+    Trace buffer data must be the only thing output on this serial port.
+    """
     def __init__(self, serial_port: serial.Serial):
+        """Initializes the serial port trace reader.
+        
+        Args:
+          serial_port: An already open and configured serial port. This should
+                       already be set up with the proper settings for baud
+                       rate, stop bits, flow control and so forth.
+        """
         self.ser = serial_port
 
     def read_next(self) -> int:
+        """Return the next value from the trace buffer and return it as an
+        integer.
+
+        This function reads one line from the serial port. If the serial port's
+        RX buffer is empty, it blocks until the next value is available.
+
+        Returns:
+          The next value from the trace buffer.
+        """
         line = self.ser.read_until()
         line = line.decode(encoding='utf-8')
         value = int(line, 0)
         return value
 
 def live_trace(reader, functions, variables, registers):
+    """Start an execution tracer live trace on the selected serial port.
+    
+    Continuously reads trace values from the serial port and converts them to
+    human readable on stdout.
+
+    The user must use ^C to terminate this function.
+
+    Args:
+      reader: A SerialPortTraceReader object for retrieving trace buffer
+              values.
+      functions: Dictionary that maps MCU addresses to function names.
+      variables: Dictionary that maps MCU addresses to variable names.
+      registers: Dictionary that maps MCU addresses to
+                 parse_svd.PeripheralRegister objects.
+  
+    """
     tracer = ExecTraceParser(functions, variables, registers)
     tracer.set_flash_base(FLASH_BASE)
     tracer.set_ram_base(RAM_BASE)
@@ -29,6 +84,11 @@ def live_trace(reader, functions, variables, registers):
     tracer.read_and_trace_all(reader)
 
 def main():
+    """Start a live exeuction trace for an embedded target using the selected
+    serial port.
+
+    See module comment for usage.
+    """
     global ser
 
     parser = argparse.ArgumentParser(description='GNU Map file parser')
@@ -69,10 +129,12 @@ def main():
     live_trace(reader, functions, variables, registers)
 
 class InputError(RuntimeError):
+    """Boilerplate code for using this file directly from the command line."""
     def __init__(self, e):
         super(InputError, self).__init__(e)
 
 if __name__ == '__main__':
+    """Boilerplate code for using this file directly from the command line."""
     try:
         main()
     except InputError as e:
