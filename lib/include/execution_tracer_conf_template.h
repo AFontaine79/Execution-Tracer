edac32ae1130ec04505ca38ea8f12d3c9dec97f9
Aaron Fontaine <AFontaine79@yahoo.com>
2023-04-07 19:15:24 -0500

    Added options to CMake to use template configuration file if none is provided and apply default settings

--- lib/include/execution_tracer_conf_template.h
+++ lib/include/execution_tracer_conf_template.h
@@ -13,29 +13,59 @@
  * offsets from the start of flash.  FLASH_BASE should equal the start of
  * FLASH space on your system.
  */
-/* Standard Arm Cortex FLASH base */
-// #define FLASH_BASE		(0x00000000)
-/* Standard STM32 FLASH base */
-#define FLASH_BASE (0x08000000)
+#define FLASH_BASE      (@EXEC_TRACE_FLASH_BASE@)
 
 /**
- * Variable and SFR addresses are also stored as offsets.
+ * RAM_BASE should equal the start of RAM space on your system.
+ * Note: Non-contiguous segments are not supported.
  */
-/* Standard Arm Cortex RAM base */
-#define RAM_BASE (0x20000000)
-/* Standard STM32 peripheral registers region base address */
-#define SFR_BASE (0x40000000)
+#define RAM_BASE        (@EXEC_TRACE_RAM_BASE@)
+
+/**
+ * SFR_BASE should equal the start of the peripheral registers area on
+ * your system.
+ * Note: Non-contiguous segments are not supported.
+ */
+#define SFR_BASE        (@EXEC_TRACE_SFR_BASE@)
 
 /**
  * Execution tracer settings.
  */
+#define ON              1
+#define OFF             0
 
-#define STOP_TRACING_AFTER_RESET        1
+/**
+ * When enabled, execution tracing will be halted by the second call to
+ * TRACE_Init() (i.e. when the system finds itself going through the startup
+ * sequence again.)  Trace entries that were not captured prior to the reset
+ * can still be traced using void DumpExecTraceLog().
+ * REQUIRES USE_NOINIT_RAM_FOR_TRACING to be enabled.
+ * NOT YET IMPLEMENTED.
+ */
+#define STOP_TRACING_AFTER_RESET        (@EXEC_TRACE_STOP_TRACING_AFTER_RESET@)
 
-#define USE_NOINIT_RAM_FOR_TRACING      1
+/**
+ * Place the trace buffer and control structure in noinit RAM.  This allows
+ * persistence through resets and may be necessary for capturing trace info
+ * prior to unexpected resources (hard fault, watchdog, etc).
+ */
+#define USE_NOINIT_RAM_FOR_TRACING      (@EXEC_TRACE_USE_NOINIT@)
 
-#define ALLOW_OVERWRITE                 1
+/**
+ * Determines what to do if the trace buffer is full when TRACE macros are
+ * called.
+ * - If enabled, the oldest trace entry will be overwritten.
+ * - If disabled, the requested trace will not be performed.
+ * Regardless of this setting, DumpExecTraceLog() will indicate that the
+ * buffer reached maximum capacity.  This should be taken as indication
+ * that entries were dropped.
+ */
+#define ALLOW_OVERWRITE                 (@EXEC_TRACE_ALLOW_OVERWRITE@)
 
-#define BUFFER_LENGTH_IN_WORDS          1024
+/**
+ * The number of trace entries that can be held in the trace buffer at once.
+ * MUST BE A POWER OF 2.
+ */
+#define BUFFER_LENGTH_IN_WORDS          (@EXEC_TRACE_NUM_TRACE_ENTRIES@)
 
 #endif /* LIB_INCLUDE_EXECUTION_TRACER_CONF_H_ */
